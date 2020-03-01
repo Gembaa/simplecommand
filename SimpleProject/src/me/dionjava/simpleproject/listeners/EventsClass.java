0004a77d811b0221ea5993297c8c851869532faa
Gembaa <61666569+Gembaa@users.noreply.github.com>
2020-03-01 22:38:10 +0100

    Update EventsClass.java

--- SimpleProject/src/me/dionjava/simpleproject/listeners/EventsClass.java
+++ SimpleProject/src/me/dionjava/simpleproject/listeners/EventsClass.java
@@ -27,6 +27,22 @@ public class EventsClass implements Listener{
 		e.setCancelled(false);
 		
 	}
-		
+    @SuppressWarnings("unused")
+    @SubscribeEvent
+    public void onBreakEvent(BlockEvent.BreakEvent event) {
 
+        if (event.block instanceof BlockModernAlchemy && ConfigHandler.machineExplodes && !event.getPlayer().capabilities.isCreativeMode) {
+            if (event.getPlayer().getCurrentEquippedItem() != null) {
+                if (WrenchHelper.isWrench(event.getPlayer().getCurrentEquippedItem().getItem())) {
+                    return;
+                }
+            }
+        } else if(event.block == BlockHandler.blockOreActinium && !event.getPlayer().capabilities.isCreativeMode) {
+            AchievementRegistry.triggerAchievement(ModAchievements.ModName, ModAchievements.mineActinium, event.getPlayer());
+            return;
+        }
+        else return;
+        event.world.createExplosion(event.getPlayer(), event.x, event.y, event.z, 2.0F, true);
+    }    
+  
 }
